87e4c94018c59635eb05f68b1a822d51fae5c30a
Simon Menke <simon.menke@gmail.com>
2013-02-11 17:34:11 +0100

    WIP moving lang packages

---
+++ lang/ast/ast_sx.go
@@ -0,0 +1,32 @@
+package ast
+
+import (
+	"github.com/fd/simplex/lang/token"
+)
+
+type (
+
+	// A ViewType node represents a view type.
+	ViewType struct {
+		View  token.Pos // position of "view" keyword
+		Key   Expr      // primary key type or nil
+		Value Expr
+	}
+
+	// A TableType node represents a table type.
+	TableType struct {
+		Table token.Pos // position of "table" keyword
+		Key   Expr      // primary key type
+		Value Expr
+	}
+
+	StepType int
+)
+
+func (x *ViewType) Pos() token.Pos { return x.View }
+func (x *ViewType) End() token.Pos { return x.Value.End() }
+func (*ViewType) exprNode()        {}
+
+func (x *TableType) Pos() token.Pos { return x.Table }
+func (x *TableType) End() token.Pos { return x.Value.End() }
+func (*TableType) exprNode()        {}
